51ea8953d7ddeda544229fc95bac5a5edccd6022
eliezermoraes <eliezermoraes7@gmail.com>
2018-09-08 22:18:49 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,8 +16,28 @@ require_once("config.php");
 //echo json_encode($search);
 
 //carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "122070");
+//echo $usuario;
+
+//$aluno = new Usuario();
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("12201970");
+//$aluno->insert();
+//echo $aluno;
+//
+//Criando um novo Usuário
+/*
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
 $usuario = new Usuario();
-$usuario->login("root", "122070");
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%&*");
 
 echo $usuario;
 
